af26236317799618ca4f82deac359ab0f39f92db
Cody <byrnes.cody@gmail.com>
2013-04-30 15:18:14 -0700

    Fight system is doneskies

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,11 +1,8 @@
 /* Prototyping */
-var blocker = document.getElementById('touch');
-var player1 = pickCharacter('one',"Lumberjack One");
-var player2 = pickCharacter('two',"Lumberjack Two");
-var attackMode;
-var tapToBlock = Hammer(blocker).on("tap", function(e){block();});
-var blockCounter = 0;
-var attackCounter = 0;
+
+//This will be set over sockets
+var player1 = pickCharacter(1,"Lumberjack One");
+var player2 = pickCharacter(2,"Lumberjack Two");
 
 function pickCharacter(player,characterName){
     var Player = {
@@ -15,34 +12,106 @@ function pickCharacter(player,characterName){
     }
     return Player;
 }
+//--------------------------------
 
 
+
+//Setting up some variables and listeners
+var blocker = document.getElementById('block');
+var attacker = document.getElementById('attack');
+var attackMode;
+
+var tapToBlock = Hammer(blocker).on("tap", function(e){block();});
+var tapToAttack = Hammer(attacker).on("tap", function(e){attack();});
+var blockCounter = 0;
+var attackCounter = 0;
+
+
+
+
+//Starts the fight sequence
 function fight(attacker, blocker){
-    attack(blocker);
-
+    swing(attacker, blocker);
 }
 
-function attack(player){
-    block();
+
+//Starts the fight timer and actions
+function swing(attacker, blocker){
+    console.log("FIGHT");
+    setFightListeners();
     attackMode = setTimeout(function(){
-        tapToBlock = null;
-        allocateDamage(player);
+        if (blockCounter > attackCounter){
+            if ((blockCounter - attackCounter) >= 10 ){
+                stopFightListeners();
+                parried(attacker);
+
+            } else {
+                stopFightListeners();
+                blocked();
+            }
+        } else if (attackCounter > blockCounter){
+            stopFightListeners();
+            hit(blocker);
+        }
 
     },5000);
 }
 
-function block(){
-        tapToBlock.enable(true);
-        blockCounter++;
-        console.log(blockCounter);
-        if (blockCounter >= 10){
-            clearInterval(attackMode);
-            tapToBlock.enable(false);
-            blockCounter = 0;
-        }
+function setFightListeners(){
+    attackCounter = 0;
+    blockCounter = 0;
+    tapToBlock.enable(true);
+    tapToAttack.enable(true);
+}
+function stopFightListeners(){
+    tapToBlock.enable(false);
+    tapToAttack.enable(false);
+    attackCounter = 0;
+    blockCounter = 0;
 }
 
+//Increases the block meter
+function block(){
+    blockCounter++;
+    console.log("BLOCK: " +blockCounter);
+}
+
+
+//Increases the attack meter
+function attack(){
+    attackCounter++;
+    console.log("ATTACK: " + attackCounter);
+}
+
+//Actions for a succesful hit
+function hit(blocker){
+    console.log("HIT");
+    allocateDamage(blocker);
+}
+
+//Actions for a successful block
+function blocked(){
+    console.log("BLOCKED");
+}
+
+//Actions for a successful parry
+function parried(attacker){
+    console.log("PARRIED");
+    allocateDamage(attacker);
+}
+
+//Checks to see if player is alive
+function checkDead(player){
+    var life = player.health;
+    if (life <= 0){
+        console.log("PLAYER "+player.playerNum + " IS DEAD");
+    }
+}
+
+//Distributes damage to a player and checks their living status
 function allocateDamage(player){
     player.health -= 1;
     console.log(player);
+    checkDead(player);
 }
+
