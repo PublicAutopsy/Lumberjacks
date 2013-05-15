1584faa94c90d12b8e1f53dfcffadd4bf319039d
Cody <byrnes.cody@gmail.com>
2013-05-15 09:26:47 -0700

    Fixing server side fight

--- app.js
+++ app.js
@@ -32,6 +32,53 @@ io.sockets.on('connection', function (socket) {
     var blockCount =0;
     var attackCount=0;
 
+    function swing(attacker, blocker){
+        console.log("FIGHT");
+        setFightListeners();
+        attackMode = setTimeout(function(){
+            if (blockCounter > attackCounter){
+                if ((blockCounter - attackCounter) >= 10 ){
+                    stopFightListeners();
+                    parried(attacker);
+
+                } else {
+                    stopFightListeners();
+                    blocked();
+                }
+            } else if (attackCounter > blockCounter){
+                stopFightListeners();
+                hit(blocker);
+            }
+
+        },5000);
+    }
+
+    function setFightListeners(){
+        attackCounter = 0;
+        blockCounter = 0;
+        socket.on('blockAttempt', function(block){
+            console.log(block);
+            blockCount += block.blockNum;
+            console.log(blockCount);
+            io.sockets.emit('blockNum', blockCount);
+        });
+
+        socket.on('attackAttempt', function(attack){
+            console.log(attack);
+            attackCount += attack.attackNum;
+            console.log(attackCount);
+            io.sockets.emit('attackNum', attackCount);
+        });
+    }
+    function stopFightListeners(){
+        socket.removeAllListeners('blockAttempt');
+
+        socket.removeAllListeners('attackAttempt');
+        attackCounter = 0;
+        blockCounter = 0;
+    }
+
+
     socket.on('blockAttempt', function(block){
         console.log(block);
         blockCount += block.blockNum;
